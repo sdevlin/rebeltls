a3bb3fcb3ae41b58234c46da95dae1e8f4543d41
Sean Devlin <sean@matasano.com>
2014-12-27 11:38:08 -0600

    added some documentation to bdlang.h

--- src/bdlang.h
+++ src/bdlang.h
@@ -1,6 +1,32 @@
 #ifndef REBELTLS_BDLANG_H
 #define REBELTLS_BDLANG_H
 
+/*
+  bdlang documentation:
+
+    endianness:
+      < - big-endian
+      > - little-endian
+      ! - network-endian (same as big-)
+      = - native-endian (almost certainly little-)
+
+    integer types:
+      b - i8
+      B - u8
+      h - i16
+      H - u16
+      T - u24
+      l - i32
+      L - u32
+      q - i64
+      Q - u64
+
+    command types:
+      register - individually specified locations
+      array    - fixed-size buffer
+      vector   - variable-size buffer (like pascal string)
+*/
+
 #include "inttypes.h"
 
 enum {
